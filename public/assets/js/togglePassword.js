8b307b1fc7bfba54932f6bd93313eb712de22440
4ur37i0 <viciego190@gmail.com>
2025-05-27 22:11:29 -0600

    Add comment to the function, that is it

--- public/assets/js/togglePassword.js
+++ public/assets/js/togglePassword.js
@@ -1,5 +1,11 @@
+/*
+ * Cualsea la pagina en la que quiera ser utilizada esta funcion
+ * debe cumplir las siguientes especificaciones: 
+ *  + Ser aplicada en una etiqueta html <input>
+ *  + Esta etiqueda debe contener la clase "password" 
+ */
 
-const checkbox = document.getElementById("checkbox");
+const checkbox = document.getElementById("checkbox"); 
 const inputsPassword = document.getElementsByClassName('password');
 
 const togglePassword = () => {
